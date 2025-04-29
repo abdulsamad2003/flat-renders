90a2cdd9e25ac6e58b695037111634e5c9f76863
user <samadsufiyan777888666@gmail.com>
2025-04-29 20:13:44 +0530

    first commit

---
+++ src/components/FlatsDetailsPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './FlatsDetailsPage.scss';
+const FlatsDetailsPage = ({flat, onClose}) => {
+  
+  return (
+    <div className="full-modal">
+      <div className="modal-content">
+      <button onClick={onClose}>Close</button>
+      <h1>Flat ID: {flat.name}</h1>
+        <p>Area: {flat.area}</p>
+        <p>BHK: {flat.bhk}</p>
+        <p>Floor: {flat.floor}</p>
+        {/* Add more flat details here as needed */}
+      </div>
+    </div>
+  );
+};
+
+export default FlatsDetailsPage;
